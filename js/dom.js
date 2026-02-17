7cb1519b6cd1a4c2a9c5328dfdb64f42ea71a8d0
vikasthakurr <vikaskumar20012001@gmail.com>
2026-02-17 21:21:14 +0530

    notes

---
+++ js/dom.js
@@ -0,0 +1,24 @@
+// console.log(this);
+
+// "use strict";
+// const abc = () => {
+//   console.log(this);
+// };
+// abc();
+
+// function abc() {
+//   let name = "vikas";
+//   console.log(this.name);
+// }
+// abc();
+
+// let obj = {
+//   name: "vikas",
+//   print: () => {
+//     console.log(this.name);
+//   },
+// };
+// obj.print();
+
+// console.log(this === window);
+
---
+++ nodejs/index.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+console.log("object");
+console.log("hiii");
+console.log("object")
