fecb0d017d5842186556957d68e50341d963eeb0
vikasthakurr <vikaskumar20012001@gmail.com>
2026-02-28 17:12:51 +0530

    redux code

--- react/redux/src/redux/reducers/TodoSlice.js
+++ react/redux/src/redux/reducers/TodoSlice.js
@@ -13,13 +13,25 @@ const todoSlice = createSlice({
       state.push(newTodo);
     },
 
-    //todo
-    //remove todo
-    //update todo
-    //delete all
+    updateTodo: (state, action) => {
+      const { id, text, completed } = action.payload;
+      const todo = state.find((item) => item.id === id);
+      if (todo) {
+        if (text !== undefined) todo.text = text;
+        if (completed !== undefined) todo.completed = completed;
+      }
+    },
+
+    deleteTodo: (state, action) => {
+      return state.filter((item) => item.id !== action.payload.id);
+    },
+
+    removeAll: () => {
+      return [];
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, updateTodo, deleteTodo, removeAll } = todoSlice.actions;
 export default todoSlice.reducer;
